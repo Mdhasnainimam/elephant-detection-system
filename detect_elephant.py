e1e65621d8c72304a30cb1d788852162a461e8f4
md hasnain imam <hasnainimam@gmail.com>
2026-02-21 12:30:41 +0530

    Fix crash issue and update elephant detection worker

--- detect_elephant.py
+++ detect_elephant.py
@@ -7,10 +7,10 @@ from ultralytics import YOLO
 # =====================
 # CONFIG
 # =====================
-RTSP_URL = "rtsp://admin:Admin%402026@192.168.68.110:554"
+RTSP_URL = "rtsp://admin:Admin%402026@192.168.1.100:554"
 
-ESP32_ALERT_URL = "http://192.168.68.112/alert"
-ESP32_CLEAR_URL = "http://192.168.68.112/clear"
+ESP32_ALERT_URL = "http://192.168.1.103/alert"
+ESP32_CLEAR_URL = "http://192.168.1.103/clear"
 
 CONFIDENCE = 0.30
 FRAME_SKIP = 2
